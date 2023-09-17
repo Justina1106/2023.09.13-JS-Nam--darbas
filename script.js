964cd5ab83489a84cebe2b156ba0dc5c9b12cace
Justina1106 <jbycenkova@gmail.com>
2023-09-17 11:42:00 +0300

    John Doe (amžius 25 m.)

---
+++ script.js
@@ -0,0 +1,13 @@
+var vardas = 'John' 
+let pavarde = 'Doe' 
+let miestas = 'Kaunas' 
+let amzius = 25
+const asmensKodas = 37878787878
+
+//console.log(vardas)
+//console.log(pavarde)
+//console.log(miestas)
+//console.log(amzius)
+//console.log(asmensKodas)
+
+console.log(vardas + ' ' + pavarde + ' ' + '(amzius' + ' ' + amzius + ' ' + 'm.), asmens kodas:' + ' ' + asmensKodas + ',' + ' ' + 'gyvenamasis miestas:' + ' ' + miestas + '.')
